86c9bb728dbd35800279efadc3801581aab468da
yq18508404250 <39258266+yq18508404250@users.noreply.github.com>
2019-04-20 18:21:01 +0800

    -算法

---
+++ Functions/BLL/GetAllOrderinfo.cs
@@ -0,0 +1,85 @@
+﻿using EFModle.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EFModle;
+
+namespace Functions.BLL
+{
+    public static class GetAllOrderinfo
+    {
+        public static TaskList task(decimal packagetask)
+        {
+            using (Entities et = new Entities())
+            {
+                var value = et.T_PACKAGE_TASK.Where(x => x.PACKTASKNUM == packagetask).FirstOrDefault();
+                var result = et.V_PRODUCE_PACKAGEINFO.Where(x => x.BILLCODE == value.BILLCODE).FirstOrDefault();
+                TaskList tasklist = new TaskList();
+                tasklist.SORTNUM = result.TASKNUM;
+                tasklist.REGIONCODE = result.REGIONCODE;
+                tasklist.SORTSEQ = result.SORTSEQ ?? 0;
+                tasklist.ORDERPACKAGEQTY = result.ORDERQUANTITY ?? 0;
+                tasklist.CUSTOMERNAME = result.CUSTOMERNAME;
+                tasklist.CUSTOMERCODE = result.CUSTOMERCODE;
+                tasklist.ALLQTY = result.ORDERQUANTITY ?? 0;
+                tasklist.ALLPACKAGENUM = value.ALLPACKAGESEQ ?? 0;
+                tasklist.PACKAGENUM = value.PACKAGESEQ ?? 0;
+                return tasklist;
+            }
+
+        }
+        public static decimal GetMaxAllpackageseq()
+        {
+            using (Entities et = new Entities())
+            {
+                return et.T_PACKAGE_TASK.Max(x => x.ALLPACKAGESEQ).Value;
+            }
+        }
+        public static decimal GetMinAllpackageseq()
+        {
+            using (Entities et = new Entities())
+            {
+                return et.T_PACKAGE_TASK.Min(x => x.ALLPACKAGESEQ).Value;
+            }
+        }
+        /// <summary>
+        /// 根据订单号来查询具体数据
+        /// </summary>
+        /// <param name="qureystr">任务号</param>
+        /// <returns></returns>
+        public static List<T_PACKAGE_TASK> QueryBypacknum(decimal allpacknum)
+        {
+            List<T_PACKAGE_TASK> taskLists = new List<T_PACKAGE_TASK>();
+            using (Entities et = new Entities())
+            {
+                var lists = et.T_PACKAGE_TASK.Where(x => x.ALLPACKAGESEQ == allpacknum).Select(x => x).ToList();
+
+                return lists;
+            }
+        }
+        public static string[] GetLabelData(decimal seq)
+        {
+            string[] str = new string[10];
+            using (Entities et = new Entities())
+            {
+                var data = et.T_PACKAGE_TASK.Where(x => x.ALLPACKAGESEQ == seq).ToList();
+                str[0] = data.Select(x => x.ALLPACKAGESEQ).FirstOrDefault().ToString();
+                str[1] = data.Select(x => x.PACKAGESEQ).FirstOrDefault().ToString();
+                str[2] = data.Where(x => x.CIGTYPE == "1").Sum(x => x.NORMALQTY).ToString();
+                str[3] = data.Where(x => x.CIGTYPE == "2").Sum(x => x.NORMALQTY).ToString();
+
+                decimal sortnum = data.Select(x => x.SORTNUM).FirstOrDefault() ?? 0;
+                var data2 = et.V_PRODUCE_PACKAGEINFO.Where(x => x.TASKNUM == sortnum).FirstOrDefault();
+                str[4] = data2.TASKNUM.ToString();
+                str[5] = data2.REGIONCODE.ToString();
+                str[6] = data2.SORTSEQ.ToString();
+                str[7] = data2.ORDERQUANTITY.ToString();
+                str[8] = data2.CUSTOMERCODE.ToString();
+                str[9] = data2.CUSTOMERNAME.ToString();
+            }
+            return str;
+        }
+    }
+}
--- PackageMachine/Fm_Orderinfo_All.cs
+++ PackageMachine/Fm_Orderinfo_All.cs
@@ -20,153 +20,346 @@ namespace PackageMachine
         /// 整个订单数据
         /// </summary>
         List<EFModle.T_PACKAGE_TASK> data1 = new List<EFModle.T_PACKAGE_TASK>();
-
         /// <summary>
-        /// 当前包序
+        /// 当前包序(整体)
         /// </summary>
         decimal SORTSEQ;
         /// <summary>
         /// 订单总包数
         /// </summary>
         decimal ORDERPACKAGEQTY;
+        /// <summary>
+        /// 订单内最大整体序号
+        /// </summary>
+        int MaxAllpackageseq;
+        /// <summary>
+        /// 订单内最小整体序号
+        /// </summary>
+        int MinAllpackageseq;
 
-        public Fm_Orderinfo_All()
+        int pkIndex = 1;
+        public Fm_Orderinfo_All(decimal package)
         {
-            //this.task = task;
+            this.task = GetAllOrderinfo.task(package);
             InitializeComponent();
-
-            //ORDERPACKAGEQTY = task.ORDERPACKAGEQTY;
-            //SORTSEQ = task.SORTSEQ;
-
-            //label_sortnum.Text = "任务号：" + task.SORTNUM;
-            //label_regioncode.Text = "车组号：" + task.REGIONCODE;
-            //label_sortseq.Text = "户序：" + task.SORTSEQ.ToString();
-            //label_customcode.Text = "专卖证号：" + task.CUSTOMERCODE;
-            //label_packnum.Text = "总包数：" + task.ORDERPACKAGEQTY.ToString();
-            //label_customername.Text = "客户名称：" + task.CUSTOMERNAME;
-            //label_allpacksortnum.Text = "总条数：" + task.ALLQTY;
-            //label_packageseq.Text = "当前第：" + task.SORTSEQ + "包";
             br = new BillResolution(cigrShow1.Size);
+
+            ORDERPACKAGEQTY = task.ORDERPACKAGEQTY;
+
+            label_sortnum.Text = "任务号：" + task.SORTNUM;
+            label_regioncode.Text = "车组号：" + task.REGIONCODE;
+            label_sortseq.Text = "户序：" + task.SORTSEQ.ToString();
+            label_customcode.Text = "专卖证号：" + task.CUSTOMERCODE;
+            label_SumCignum.Text = "订单总条数：" + task.ORDERPACKAGEQTY.ToString();
+            label_customername.Text = "客户名称：" + task.CUSTOMERNAME;
+            label_allpacksortnum.Text = "总条数：" + task.ALLQTY;
+            label_packageseq.Text = "当前订单第：" + task.PACKAGENUM + "包";
+
             //加载整个订单数据
             data1 = FmOrderInofFun.QueryBySortnum(task.SORTNUM);
+            MaxAllpackageseq = (int)GetAllOrderinfo.GetMaxAllpackageseq();
+            MinAllpackageseq = (int)GetAllOrderinfo.GetMinAllpackageseq();
+
+            label_allpackageseq.Text = "当前包装机共：" + br.Length + "包";
+            label_nowpackageseq.Text = "当前包装机第：" + MinAllpackageseq + "包";
 
         }
-         
+
+        BillResolution br;
+        private void Fm_Orderinfo_All_Load(object sender, EventArgs e)
+        {
+            pkIndex = MinAllpackageseq;
+            BindBillInfo(pkIndex);
+            GetValues();
+        }
+        /// <summary>
+        /// DataType： GetValues类型 整包1  /常规2  /异型3  /整个订单4
+        /// </summary>
+        int DataType = 1;
+        /// <summary>
+        /// 获取数据绑定 数据控件datagridview 
+        /// </summary>
+        public void GetValues(bool tag = false)
+        {
+            if (tag)
+            {
+                return;
+            }
+            SORTSEQ = pkIndex;
+             FmOrderInofFun.QueryBySortnum(task.SORTNUM);
+            //获最大包数 最小包数
+            if (SORTSEQ <= MaxAllpackageseq && SORTSEQ >= MinAllpackageseq)
+            {
+                switch (DataType)
+                {
+                    case 1:
+                        
+                        Dgv_datainfo.DataSource = GetAllOrderinfo.QueryBypacknum(SORTSEQ).OrderBy(x => x.CIGNUM).Select(x => new { x.CIGARETTENAME, x.CIGARETTECODE, CIGTYPE = x.CIGTYPE == "1" ? "常规烟" : "异型烟", x.NORMALQTY, x.PACKAGESEQ }).ToList();
+                        break;
+                    case 2:
+                        Dgv_datainfo.DataSource = GetAllOrderinfo.QueryBypacknum(SORTSEQ).OrderBy(x => x.CIGNUM).Select(x => new { x.CIGARETTENAME, x.CIGARETTECODE, CIGTYPE = x.CIGTYPE == "1" ? "常规烟" : "异型烟", x.NORMALQTY, x.PACKAGESEQ }).ToList();
+                        break;
+                    case 3:
+                        Dgv_datainfo.DataSource = GetAllOrderinfo.QueryBypacknum(SORTSEQ).OrderBy(x => x.CIGNUM).Select(x => new { x.CIGARETTENAME, x.CIGARETTECODE, CIGTYPE = x.CIGTYPE == "1" ? "常规烟" : "异型烟", x.NORMALQTY, x.PACKAGESEQ }).ToList();
+                        break;
+                    case 4:
+                        Dgv_datainfo.DataSource = GetAllOrderinfo.QueryBypacknum(SORTSEQ).Select(x => new { x.CIGARETTENAME, x.CIGARETTECODE, CIGTYPE = x.CIGTYPE == "1" ? "常规烟" : "异型烟", x.NORMALQTY, x.PACKAGESEQ }).ToList();
+                        break;
+                    default:
+                        break;
+                }
+
+                this.Dgv_datainfo.AutoGenerateColumns = true;
+                Dgv_datainfo.Columns[0].HeaderText = "卷烟名称";
+                Dgv_datainfo.Columns[0].Width = 150;
+                Dgv_datainfo.Columns[1].HeaderText = "卷烟编码";
+                Dgv_datainfo.Columns[1].Width = 90;
+                Dgv_datainfo.Columns[2].HeaderText = "条烟类型";
+                Dgv_datainfo.Columns[2].Width = 90;
+                Dgv_datainfo.Columns[3].HeaderText = "烟条数";
+                Dgv_datainfo.Columns[3].Width = 80;
+                Dgv_datainfo.Columns[4].HeaderText = "包序号";
+                Dgv_datainfo.Columns[4].Width = 80;
+            }
+            label_allcig.Text = data1.Where(x => x.ALLPACKAGESEQ == pkIndex).Select(x => x.PACKAGEQTY).FirstOrDefault() + "条烟";
+        }
         /// <summary>
         /// 获取数据绑定 数据控件datagridview
         /// </summary>
-        /// <param name="falg">操作类型：1上一包，2下一包，3第一包，4最后一包</param>
-        public void getvalues(int falg)
+        public void GetValuesAll()
         {
-            switch (falg)
+            SORTSEQ = pkIndex;
+            //获最大包数 最小包数
+            if (SORTSEQ <= MaxAllpackageseq && SORTSEQ >= MinAllpackageseq)
             {
-                case 1:
-                    //上一包
-                    SORTSEQ--;
-                    break;
-                case 2:
-                    //下一包
-                    SORTSEQ++;
-                    break;
-                case 3:
-                    //第一包
-                    SORTSEQ = 1;
-                    break;
-                case 4:
-                    //最后一包
-                    SORTSEQ = ORDERPACKAGEQTY;
-                    break;
-            }
-            //获取总包数
-            if (SORTSEQ <= ORDERPACKAGEQTY && SORTSEQ > 0)
-            {
-                Dgv_datainfo.DataSource = data1.Where(x => x.PACKAGESEQ == SORTSEQ).OrderBy(x => x.CIGNUM).Select(x => new { x.CIGARETTENAME, x.CIGARETTECODE, x.CIGNUM });
-                Dgv_datainfo.ColumnHeadersVisible = true;
+
+                Dgv_datainfo.DataSource = data1.Where(x => x.ALLPACKAGESEQ == SORTSEQ).Select(x => new { x.CIGARETTENAME, x.CIGARETTECODE, x.CIGTYPE, x.NORMALQTY, x.PACKAGESEQ }).ToList();
+                this.Dgv_datainfo.AutoGenerateColumns = true;
+                //string sss = Dgv_datainfo.Columns.Count.ToString();
+                //string rrr = Dgv_datainfo.Rows.Count.ToString();
                 Dgv_datainfo.Columns[0].HeaderText = "卷烟名称";
-                Dgv_datainfo.Columns[0].Width = 300;
+                Dgv_datainfo.Columns[0].Width = 150;
                 Dgv_datainfo.Columns[1].HeaderText = "卷烟编码";
-                Dgv_datainfo.Columns[1].Width = 105;
-                Dgv_datainfo.Columns[2].HeaderText = "条烟流水号";
-                Dgv_datainfo.Columns[2].Width = 105;
+                Dgv_datainfo.Columns[1].Width = 90;
+                Dgv_datainfo.Columns[2].HeaderText = "条烟类型";
+                Dgv_datainfo.Columns[2].Width = 90;
+                Dgv_datainfo.Columns[3].HeaderText = "烟条数";
+                Dgv_datainfo.Columns[3].Width = 80;
+                Dgv_datainfo.Columns[4].HeaderText = "包序号";
+                Dgv_datainfo.Columns[4].Width = 80;
             }
+            label_allcig.Text = data1.Where(x => x.ALLPACKAGESEQ == pkIndex).Select(x => x.PACKAGEQTY).FirstOrDefault() + "条烟";
         }
 
-
-
-
-        int pkIndex = 1;
-        public decimal GetLeng
-        {
-            get => br.Length;
-        }
-        private void button2_Click(object sender, EventArgs e)
-        {
-            if (pkIndex <= GetLeng)
-            {
-                pkIndex++;
-                if (GetLeng >= pkIndex)
-                {
-                    BindBillInfo(pkIndex);
-                }
-                else
-                {
-                    pkIndex = int.Parse(GetLeng.ToString());
-                    MessageBox.Show("已经是最后一个订单的了");
-                }
-
-            }
-        }
 
         private void cigrShow1_Load(object sender, EventArgs e)
         {
 
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void Dgv_datainfo_RowStateChanged(object sender, DataGridViewRowStateChangedEventArgs e)
         {
-            if (pkIndex >= 1)
-            {
-                pkIndex--;
-                if (pkIndex >= 1)
-                {
-
-                    BindBillInfo(packageIndex: pkIndex);
-                }
-                else
-                {
-                    pkIndex = 1;
-                    MessageBox.Show("已经是第一个订单的了");
-                }
-
-            }
+            e.Row.HeaderCell.Value = string.Format("{0}", e.Row.Index + 1);
         }
-        BillResolution br = null;
+
+
         void BindBillInfo(int packageIndex = 0, int CinNum = 0)
         {
 
-            List<TobaccoInfo> list = br.GetTobaccoInfoss(packageIndex, cigrShow1.Height); 
+            List<TobaccoInfo> list = br.GetTobaccoInfoss(packageIndex, cigrShow1.Height);
             cigrShow1.UpdateValue(list);
-            label_sortnum.Text = "任务号："+list.Select(x => x.SortNum).FirstOrDefault().ToString();
-            label_billcode.Text = "订单号："+list.Select(x => x.BillCode).FirstOrDefault();
-            CustomerModle customerModles = br.GetCustomerInfos(list.Select(x => x.BillCode).FirstOrDefault());
-
         }
-
-        private void button5_Click(object sender, EventArgs e)
+        private void button_last_Click(object sender, EventArgs e)
         {
-            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            if (pkIndex >= MinAllpackageseq)
             {
-                pkIndex = int.Parse(textBox1.Text);
-                BindBillInfo(pkIndex);
+                if (pkIndex > MinAllpackageseq)
+                {
+                    pkIndex--;
+                    BindBillInfo(packageIndex: pkIndex);
+                    if (checkBox_display.Checked)
+                    {
+                        GetValues(true);
+                    }
+                    else
+                    {
+                        GetValues();
+                    }
+                    labelChange();
+                }
+                else
+                {
+                    MessageBox.Show("已经是单内第一个订单了");
+                }
+
             }
         }
 
-        private void Fm_Orderinfo_All_Load(object sender, EventArgs e)
+        private void button_next_Click(object sender, EventArgs e)
         {
-             
-            pkIndex = 1;
-            BindBillInfo(1); 
-             
+            if (pkIndex <= MaxAllpackageseq)
+            {
+                pkIndex++;
+                if (MaxAllpackageseq >= pkIndex)
+                {
+                    BindBillInfo(pkIndex);
+                    if (checkBox_display.Checked)
+                    {
+                        GetValues(true);
+                    }
+                    else
+                    {
+                        GetValues();
+                    }
+                    labelChange();
+                }
+                else
+                {
+                    pkIndex = MaxAllpackageseq;
+                    MessageBox.Show("已经是订单内最后一包了");
+                }
+            }
+        }
+
+        private void button_skip_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                try
+                {
+                    pkIndex = int.Parse(textBox1.Text) + MinAllpackageseq - 1;//订单内包序处理为包装机整体包序
+                }
+                catch (FormatException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                if (pkIndex >= MinAllpackageseq && pkIndex <= MaxAllpackageseq)//如果在订单内的包序
+                {
+                    BindBillInfo(pkIndex);
+                    labelChange();
+                    GetValues();
+                }
+                else if (pkIndex < MinAllpackageseq)//如果小于订单内的包序
+                {
+                    textBox1.Text = "1";
+                    pkIndex = MinAllpackageseq;
+                    BindBillInfo(pkIndex);
+                    labelChange();
+                    GetValues();
+                }
+                else if (pkIndex > MaxAllpackageseq)//如果大于订单内的包序
+                {
+                    textBox1.Text = (MaxAllpackageseq - MinAllpackageseq + 1).ToString();
+                    pkIndex = MaxAllpackageseq;
+                    BindBillInfo(pkIndex);
+                    labelChange();
+                    GetValues();
+                }
+            }
+        }
+
+        private void button_top_Click(object sender, EventArgs e)
+        {
+            if (pkIndex > MinAllpackageseq)
+            {
+                pkIndex = MinAllpackageseq;
+                BindBillInfo(pkIndex);
+                if (checkBox_display.Checked)
+                {
+                    GetValues(true);
+                }
+                else
+                {
+                    GetValues();
+                }
+                labelChange();
+            }
+            else
+            {
+                MessageBox.Show("已经是订单内第一包了");
+            }
+        }
+        /// <summary>
+        /// 界面label控件数据刷新
+        /// </summary>
+        void labelChange()
+        {
+            string[] task = GetAllOrderinfo.GetLabelData(SORTSEQ);
+            label_nowpackageseq.Text = "当前包装机第：" + task[0] + "包";
+            label_packageseq.Text = "当前订单第：" + task[1] + "包";
+            label_normul.Text = "常规烟：" + task[2];
+            label_unnormul.Text = "异型烟：" + task[3];
+
+            label_sortnum.Text = "任务号：" + task[4]; 
+            label_regioncode.Text = "车组号：" + task[5]; ;
+            label_sortseq.Text = "户序：" + task[6];
+            label_customcode.Text = "专卖证号：" + task[8];
+            label_SumCignum.Text = "订单总条数：" + task[7];
+            label_customername.Text = "客户名称：" + task[9];
+        }
+
+        private void button_end_Click(object sender, EventArgs e)
+        {
+            if (pkIndex < MaxAllpackageseq)
+            {
+                pkIndex = MaxAllpackageseq;
+                BindBillInfo(pkIndex);
+                if (checkBox_display.Checked)
+                {
+                    GetValues(true);
+                }
+                else
+                {
+                    GetValues();
+                }
+                labelChange();
+            }
+            else
+            {
+                MessageBox.Show("已经是订单内最后一包了");
+            }
+
+        }
+
+        private void checkBox_display_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox_display.Checked)
+            {
+                DataType = 4;
+                GetValues();
+                radioButton_all.Checked = true;
+            }
+            else
+            {
+                DataType = 1;
+                GetValues();
+            }
+        }
+
+        private void radioButton_cgy_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_cgy.Checked)
+            {
+                DataType = 2;
+                checkBox_display.Checked = false;
+                GetValues();
+            }
+        }
+        private void radioButton_yxy_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_yxy.Checked)
+            {
+                DataType = 3;
+                checkBox_display.Checked = false;
+                GetValues();
+            }
+        }
+        private void radioButton_all_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton_all.Checked)
+            {
+                DataType = 1;
+                GetValues();
+            }
         }
     }
 
